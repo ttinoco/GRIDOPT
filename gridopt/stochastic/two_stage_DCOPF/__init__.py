3ca253274f6c0c540d7ef564518d971445e1a660
Tomas Tinoco De Rubira <ttinoco5687@gmail.com>
2015-12-07 16:46:50 +0100

    made progress on risk averse problem

--- gridopt/stochastic/two_stage_DCOPF/__init__.py
+++ gridopt/stochastic/two_stage_DCOPF/__init__.py
@@ -7,6 +7,7 @@
 #*****************************************************#
 
 from problem import TS_DCOPF
+from problem_risk import TS_DCOPF_RiskAverse
 from lshaped import LShaped
 from lshaped_multi import LShapedMulticut
 
--- gridopt/stochastic/two_stage_DCOPF/problem_risk.py
+++ gridopt/stochastic/two_stage_DCOPF/problem_risk.py
@@ -8,8 +8,10 @@
 
 import numpy as np
 from utils import ApplyFunc
+from problem import TS_DCOPF
+from scipy.sparse import csr_matrix
 from multiprocessing import Pool, cpu_count
-from optalg.stoch_solver import TS_DCOPF, StochGen_Problem
+from optalg.stoch_solver import StochGen_Problem
 
 class TS_DCOPF_RiskAverse(StochGen_Problem):
     """"
@@ -29,23 +31,33 @@ class TS_DCOPF_RiskAverse(StochGen_Problem):
                         0 <= s <= r.
     """
 
-    def __init__(self,net,Qmax,gamma):
+    # Parameters
+    parameters = {'lam_max' : 1e2}   # max Lagrange multiplier
+
+    def __init__(self,net,Qfac,gamma):
         """
         Class constructor.
         
         Parameters
         ----------
         net : Network
-        Qmax : float
+        Qfac : float (> 1)
         gamma : float
         """
+
+        # Parameters
+        self.parameters = TS_DCOPF_RiskAverse.parameters.copy()
         
         # Save args
-        self.Qmax = Qmax
+        self.Qfac = Qfac
         self.gamma = gamma
         
         # Regular problem
         self.ts_dcopf = TS_DCOPF(net)
+
+        # Qmax
+        p_ce = self.ts_dcopf.solve_approx(quiet=True)
+        self.Qmax = Qfac*self.ts_dcopf.eval_Q(p_ce,self.ts_dcopf.Er)[0]
 
     def eval_FG(self,x,w,problem=None):
         """
@@ -62,7 +74,7 @@ class TS_DCOPF_RiskAverse(StochGen_Problem):
         F : float
         gF : subgradient vector
         G : vector
-        JG : matrix of subgradients
+        JG : csr_matrix of subgradients 
         """
 
         p = x[:-1]
@@ -71,7 +83,7 @@ class TS_DCOPF_RiskAverse(StochGen_Problem):
         gamma = self.gamma
 
         H0 = self.ts_dcopf.H0
-        g0 = self.ts_dcopt.g0
+        g0 = self.ts_dcopf.g0
         
         phi = 0.5*np.dot(p,H0*p)+np.dot(g0,p)
         gphi = H0*p + g0
@@ -84,9 +96,9 @@ class TS_DCOPF_RiskAverse(StochGen_Problem):
 
         G = np.array([np.maximum(sigma,0.) + (1.-gamma)*t])
         if sigma >= 0:
-            JG = np.hstack((gQ,-1. + (1.-gamma))).reshape(1,x.size)
+            JG = csr_matrix(np.hstack((gQ,-1. + (1.-gamma))),shape=(1,x.size))
         else:
-            JG = np.hstack((np.zeros(p.size),1.-gamma)).reshape(1,x.size)
+            JG = csr_matrix(np.hstack((np.zeros(p.size),1.-gamma)),shape=(1,x.size))
 
         return F,gF,G,JG
 
@@ -109,12 +121,12 @@ class TS_DCOPF_RiskAverse(StochGen_Problem):
         
         p = x[:-1]
         t = x[-1]
-        Er = self.ts_dcopt.Er
+        Er = self.ts_dcopf.Er
         
         gamma = self.gamma
 
         H0 = self.ts_dcopf.H0
-        g0 = self.ts_dcopt.g0
+        g0 = self.ts_dcopf.g0
         
         phi = 0.5*np.dot(p,H0*p)+np.dot(g0,p)
         gphi = H0*p + g0
@@ -124,10 +136,15 @@ class TS_DCOPF_RiskAverse(StochGen_Problem):
         gF = np.hstack((gphi+gQ,0.))
 
         sigma = Q-self.Qmax-t
-        C = np.exp(sigma)/(1.+np.exp(sigma))
+        if sigma > 100:
+            C = 1.
+            log_term = sigma
+        else:
+            C = np.exp(sigma)/(1.+np.exp(sigma))
+            log_term = np.log(1. + np.exp(sigma))
 
-        G = np.array([np.log(1. + np.exp(sigma)) + (1.-gamma)*t])
-        JG = np.hstack((C*gQ,-C + (1.-gamma))).reshape(1,x.size)
+        G = np.array([log_term + (1.-gamma)*t])
+        JG = csr_matrix(np.hstack((C*gQ,-C + (1.-gamma))),shape=(1,x.size))
 
         return F,gF,G,JG
 
@@ -147,7 +164,7 @@ class TS_DCOPF_RiskAverse(StochGen_Problem):
         F = 0.
         gF = np.zeros(x.size)
         G = np.zeros(1)
-        JG = np.zeros((1,x.size))
+        JG = csr_matrix((1,x.size))
         
         # Second stage problem
         problem = self.ts_dcopf.get_problem_for_Q(p,np.ones(num_r))
@@ -165,7 +182,7 @@ class TS_DCOPF_RiskAverse(StochGen_Problem):
             F += (F1-F)/(i+1.)
             gF += (gF1-gF)/(i+1.)
             G += (G1-G)/(i+1.)
-            JG += (JG1-JG)/(i+1.)
+            JG = JG + (JG1-JG)/(i+1.)
                             
         return F,gF,G,JG
         
@@ -180,7 +197,7 @@ class TS_DCOPF_RiskAverse(StochGen_Problem):
         
     def get_size_x(self):
 
-        return self.ts_dcopt.num_p + 1
+        return self.ts_dcopf.num_p + 1
 
     def get_size_lam(self):
 
@@ -202,7 +219,8 @@ class TS_DCOPF_RiskAverse(StochGen_Problem):
 
     def project_lam(self,lam):
 
-        pass
+        lmax = self.parameters['lam_max']
+        return np.maximum(np.minimum(lam,lmax),0.)
 
     def sample_w(self):
 
