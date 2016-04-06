fixed bug with dc opf and multiplier scaling

gridopt/power_flow/dc_opf.py
@@ -125,8 +125,8 @@ class DCOPF(PFmethod):
         Oz = coo_matrix((nz,nz))
         oz = np.zeros(nz)
         
-        H = bmat([[Hx,None],[None,Oz]],format='coo')/net.base_power
-        g = np.hstack((gx,oz))/net.base_power
+        H = bmat([[Hx,None],[None,Oz]],format='coo')/net.base_power # scaled
+        g = np.hstack((gx,oz))/net.base_power                       # scaled
 
         A = bmat([[Ax,None],[Gz,-Iz]],format='coo')
         b = np.hstack((bx,oz))
@@ -216,4 +216,7 @@ class DCOPF(PFmethod):
         
         # Network sensitivities
         net.clear_sensitivities()
-        problem.store_sensitivities(lam[:net.num_buses],nu,mu,pi)
+        problem.store_sensitivities(lam[:net.num_buses]*net.base_power,
+                                    nu,
+                                    mu*net.base_power,
+                                    pi*net.base_power)

gridopt/power_flow/dc_opf_mp.py
@@ -149,8 +149,8 @@ class DCOPF_MP(PFmethod):
                 Lproj.append(bmat([[Pl,coo_matrix((Pl.shape[0],nz))]]))
                 Ltot = Ltot + Pl*x 
  
-            H = bmat([[Hx,None],[None,Oz]],format='coo')/net.base_power
-            g = np.hstack((gx,oz))/net.base_power
+            H = bmat([[Hx,None],[None,Oz]],format='coo')/net.base_power # scaled
+            g = np.hstack((gx,oz))/net.base_power                       # scaled
             
             A = bmat([[Ax,None],[Gz,-Iz]],format='coo')
             b = np.hstack((bx,oz))
@@ -299,4 +299,7 @@ class DCOPF_MP(PFmethod):
         
         # Network sensitivities
         net.clear_sensitivities()
-        problem.store_sensitivities(lam[:net.num_buses],nu,mu,pi)
+        problem.store_sensitivities(lam[:net.num_buses]*net.base_power,
+                                    nu,
+                                    mu*net.base_power,
+                                    pi*net.base_power)

gridopt/power_flow/dc_opf_prev.py
@@ -177,8 +177,8 @@ class PreventiveDCOPF(PFmethod):
         Zr = coo_matrix((nr,nr))
         zr = np.zeros(nr)
         
-        H = bmat([[Hp,None],[None,Zr]],format='coo')/net.base_power
-        g = np.hstack((gp,zr))/net.base_power
+        H = bmat([[Hp,None],[None,Zr]],format='coo')/net.base_power # scaled
+        g = np.hstack((gp,zr))/net.base_power                       # scaled
 
         y = np.hstack((p,zr))
 
@@ -273,4 +273,7 @@ class PreventiveDCOPF(PFmethod):
         
         # Network sensitivities
         net.clear_sensitivities()
-        problem.store_sensitivities(lam[:net.num_buses],nu,mu,pi)
+        problem.store_sensitivities(lam[:net.num_buses]*net.base_power,
+                                    nu,
+                                    mu*net.base_power,
+                                    pi*net.base_power)