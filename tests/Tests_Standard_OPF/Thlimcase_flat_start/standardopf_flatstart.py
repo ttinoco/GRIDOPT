1d9e3537504ce5ee0efa648877212965eac9eb40
Martin Baltzinger <mbaltzinger@MacBook-Air-de-Martin.local>
2017-03-21 22:44:56 +0100

    Adding flat start comparison

--- tests/Tests_Standard_OPF/Thlimcase_flat_start/standardopf_flatstart.py
+++ tests/Tests_Standard_OPF/Thlimcase_flat_start/standardopf_flatstart.py
@@ -9,21 +9,21 @@ listiterationsc=[]
 listPowermis=[]
 listTime=[]
 listcase=['case9','case14','case39','case89pegase','case118','case300','case1354pegase','case2869pegase','case3375wp']
-listmethod =['IpoptOPF','AugLOPF']
+listmethod=['IpoptOPF','AugLOPF']
 for case in listcase:
 	i=0
-	with open('sol_wo_thlim'+case+'.csv') as f:
+	with open('sol_wo_thlim_flatstart'+case+'.csv') as f:
 	    reader = csv.reader(f)
 	    for row in reader:
 	    	
 	    	if i==0:
-	    		objfmatlab		=row[0]
+	    		objfmatlab		=float(row[0])
 	    	if i==1:
-	    		iterationmatlab = row[0]
+	    		iterationmatlab = int(row[0])
 	    	if i==2:
-	    		maxPmismatlab 	= row[0]
+	    		maxPmismatlab 	= float(row[0])
 	    	if i==3:
-	    		timematlab 		= row[0]
+	    		timematlab 		= float(row[0])
 	    	i +=1
 	net = pf.Network()
 	pfcase = 'pf'+case+'.mat'
@@ -40,6 +40,12 @@ for case in listcase:
 			start = timeit.default_timer()
 
 			method.set_parameters({'quiet': True})
+
+			for bus in net.buses:
+				bus.v_mag = 1
+				bus.v_ang = 0
+
+			net.update_properties()
 
 			method.solve(net)
 
@@ -79,6 +85,9 @@ for case in listcase:
 	listPowermis.append(listPowermisc)
 	listTime.append(listTimec)
 
-print('type of case','	----	IPOPT/AUGLOPF/MATLAB Objfunction','	----	IPOT/AUGLOPF/MATLAB iterations','	----	IPOPT/AUGLOPF/MATALB Time','	----	IPOPT/AUGLOPF/MATALB maxPowermis')
-for i in range(0,len(listiterations)):
-	print(listcase[i],'	----	',listobjf[i],'	----	',listiterations[i],'	----	',listTime[i],'	----	',listPowermis[i])
+
+with open('Solution_StandardOPF_wo_thlim_flat_start.csv', 'w', newline='') as solfile:
+    spamwriter = csv.writer(solfile)
+    spamwriter.writerow(['type of case','IPOPT Objfunction','AUGLOPF Objfunction','MATLAB Objfunction','IPOPT Iterations','AUGLOPF Iterations','MATLAB Iterations','IPOPT Time','AUGLOPF Time','MATLAB Time','IPOPT Powermis','AUGLOPF Powermis','MATLAB Powermis'])
+    for i in range(0,len(listiterations)):
+    	spamwriter.writerow([str(listcase[i]),(listobjf[i][0]),(listobjf[i][1]),(listobjf[i][2]),(listiterations[i][0]),(listiterations[i][1]),(listiterations[i][2]),(listTime[i][0]),(listTime[i][1]),(listTime[i][2]),(listPowermis[i][0]),(listPowermis[i][1]),(listPowermis[i][2])])
