51a42fb7ab2f1f3e845864a5c2b8e71704a7cdc5
Tomas Tinoco De Rubira <ttinoco5687@gmail.com>
2017-10-06 22:33:02 -0700

    removed stochastic stuff, updated setup.py and dependencies

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #*****************************************************#
 # This file is part of GRIDOPT.                       #
 #                                                     #
-# Copyright (c) 2015-2017, Tomas Tinoco De Rubira.    #
+# Copyright (c) 2015, Tomas Tinoco De Rubira.         #
 #                                                     #
 # GRIDOPT is released under the BSD 2-clause license. #
 #*****************************************************#
@@ -9,18 +9,16 @@
 from setuptools import setup
 
 setup(name='GRIDOPT',
+      zip_safe=False,
       version='1.3.3',
       description='Power Grid Optimization Library',
       author='Tomas Tinoco De Rubira',
       author_email='ttinoco5687@gmail.com',
+      license='BSD 2-Clause License',
       packages=['gridopt',
-                'gridopt.power_flow',
-                'gridopt.stochastic',
-                'gridopt.stochastic.two_stage_DCOPF',
-                'gridopt.stochastic.multi_stage_DCOPF'],
+                'gridopt.power_flow'],
       scripts=['./script/gridopt'],
-      install_requires=['scipy',
-                        'numpy',
+      install_requires=['numpy>=1.11.2',
+                        'scipy>=0.18.1',
                         'pfnet',
-                        'optalg',
-                        'multiprocess'])
+                        'optalg'])
