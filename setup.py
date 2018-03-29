f408dac539ea4b68e0a7a275ef1801db0faa1f51
Tomas Tinoco De Rubira <ttinoco5687@gmail.com>
2018-03-28 21:13:37 -0700

    updated release counts

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(name='GRIDOPT',
       zip_safe=False,
-      version='1.3.5rc1',
+      version='1.3.5rc2',
       description='Power Grid Optimization Library',
       url='https://github.com/ttinoco/GRIDOPT',
       author='Tomas Tinoco De Rubira',
@@ -26,6 +26,6 @@ setup(name='GRIDOPT',
       install_requires=['cython>=0.20.1',
                         'numpy>=1.11.2',
                         'scipy>=0.18.1',
-                        'pfnet==1.3.3rc1',
+                        'pfnet==1.3.3rc2',
                         'optalg==1.1.6rc1',
                         'nose'])
